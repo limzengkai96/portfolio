a0e9f7df4052b479098b59a4ecb10ea6e39115e3
limzengkai96 <limzengkai96@gmail.com>
2023-08-19 19:13:37 +0800

    thrid commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import NavBar from './components/Navbar';
 import Layout from './components/Layout';
 
 // import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Layout />
   );
 }
 
