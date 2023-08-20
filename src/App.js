e95a4428dcf751723ea5c040ba94edf7db841610
limzengkai96 <limzengkai96@gmail.com>
2023-08-20 13:22:02 +0800

    Test again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.scss';
-//import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import NavBar from './components/Navbar';
 import Layout from './components/Layout';
 
 // import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <Layout />
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          {/* <Route index element={<Home />}></Route> */}
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import './index.scss'
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import LogoS from '../../assets/images/kai_logo.png'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faBars } from '@fortawesome/free-solid-svg-icons';
@@ -16,11 +16,14 @@ const Sidebar = () => {
 
     return(
         <div className='nav-bar'>
-
+            <Link className='logo' to='/'>
                 <img src={LogoS} alt="logo" />
-
+            </Link>
             <div className="nav-items">
-                <h1>Home</h1>
+                <Link>
+                    <h1>Home</h1>
+                </Link>
+
                 <h1>Education</h1>
                 <h1>Experience</h1>
                 <h1>Qualification</h1>
@@ -31,10 +34,15 @@ const Sidebar = () => {
             </div>
             {show && (
                 <ul className="dropdown-menu">
-
-                    <DropdownItem text="Home" />
-                    <DropdownItem text="Education" />
-                    <DropdownItem text="Experience" />        
+                    <Link to={"/"}>
+                        <DropdownItem text="Home" />
+                    </Link>
+                    <Link>
+                        <DropdownItem text="Education" />
+                    </Link>
+                    <Link>
+                        <DropdownItem text="Experience" />        
+                    </Link>
                     <DropdownItem text="Qualification" />
                     <DropdownItem text="Project" />
                 </ul>
